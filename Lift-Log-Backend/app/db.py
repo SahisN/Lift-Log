7ef12bcb88d93d9a545c5b547b5abc431d4daf23
Sahis <sahisneupane23@gmail.com>
2026-07-26 23:15:54 -0700

    added automatic db container startup when db url if db url is missing from env

---
+++ Lift-Log-Backend/app/db.py
@@ -0,0 +1,78 @@
+import typing as t
+from functools import lru_cache
+
+from deps import inject_app_settings
+from fastapi import Depends
+from sqlalchemy.engine import make_url
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+_container: t.Any = None
+_container_sync_url: str | None = None
+
+
+def _start_testcontainer() -> str:
+    global _container, _container_sync_url
+
+    from testcontainers.postgres import PostgresContainer
+
+    _container = PostgresContainer("postgres:17-alpine")
+    try:
+        _container.start()
+    except Exception as exec:
+        raise RuntimeError(
+            "Failed to start Postgres testcontainer. Is Docker running? check (`docker ps`)"
+        ) from exec
+
+    raw_url: str = make_url(_container.get_connection_url())
+    async_url = raw_url.set(drivername="postgresql+asyncpg")
+    sync_url = raw_url.set(drivername="postgresql+psycopg")
+
+    _container_sync_url = sync_url.render_as_string(hide_password=False)
+
+    print("ASYNC_DATABASE_URL not set, starting testcontainer")
+
+    return async_url.render_as_string(hide_password=False)
+
+
+def get_container_sync_url() -> str | None:
+    return _container_sync_url
+
+
+def inject_engine() -> None:
+    settings = inject_app_settings()
+    url = settings.secrets.async_database_url.get_secret_value()
+    if not url:
+        url = _start_testcontainer()
+    return create_async_engine(url)
+
+
+@lru_cache
+def _inject_session_factory() -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(
+        inject_engine(), class_=AsyncSession, expire_on_commit=False
+    )
+
+
+def inject_session_factory() -> async_sessionmaker[AsyncSession]:
+    return _inject_session_factory()
+
+
+async def inject_postgres_session(
+    factory: async_sessionmaker[AsyncSession] = Depends(_inject_session_factory),  # noqa: B008
+) -> t.AsyncGenerator[AsyncSession, None]:
+    async with factory() as session:
+        yield session
+
+
+async def stop_testcontainer() -> None:
+    global _container, _container_sync_url
+
+    if _container is not None:
+        print("Stopping testcontainer")
+        _container.stop()
+        _container = None
+        _container_sync_url = None
--- Lift-Log-Backend/app/server.py
+++ Lift-Log-Backend/app/server.py
@@ -1,13 +1,17 @@
 from contextlib import asynccontextmanager
 
 from api.routes.routers import build_router
+from db import inject_engine, stop_testcontainer
 from exception_handler import register_exception_handlers
 from fastapi import FastAPI
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    inject_engine()
+
     yield
+    await stop_testcontainer()
 
 
 app = FastAPI(lifespan=lifespan)
